44dbe48f669358c30ae56133d5079c18d2f6827e
Brunom8218 <bmars8218@gmail.com>
2023-11-13 15:34:41 +1300

    231112.8 change the color mode

--- components/Menu/Navigation.tsx
+++ components/Menu/Navigation.tsx
@@ -34,9 +34,9 @@ const Navigation = () => {
 
   const borderColor = useColorModeValue('teal.500', 'cyan.200')
 
-  const IsLight = colorMode === ThemeMode.Light
-  const btnClassName = `${styles.blogBtn} ${!IsLight && styles.light}`
-  const Icon = IsLight ? SunIcon : MoonIcon
+  const IsDark = colorMode === ThemeMode.Light
+  const btnClassName = `${styles.blogBtn} ${!IsDark && styles.dark}`
+  const Icon = IsDark ? SunIcon : MoonIcon
   const onMenuItemClick = useCallback(
     (e) => {
       e.stopPropagation()
@@ -66,7 +66,7 @@ const Navigation = () => {
           onClick={toggleColorMode}
           padding={0}
         />
-        <MobileMenu isLightMode={IsLight} toggle={toggleOpen} isOpen={isOpen} />
+        <MobileMenu isDarkMode={IsDark} toggle={toggleOpen} isOpen={isOpen} />
       </Box>
 
       <MotionContainer
